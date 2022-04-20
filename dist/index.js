c12e5f34baad8efed8de1809d8e8791cc44fff56
Yuriy Boev <yuriy.boev@osf-commerce.com>
2022-04-20 22:28:46 +0300

    Added prettier, eslint, Github Actions logic and yml file

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,54 @@
-"use strict";
-const io = require('@actions/io');
-const fs = require('fs');
-let rawData = fs.readFileSync('contentful-export-yqiccqy-master.json');
-let exportData = JSON.parse(rawData);
-const localesToRemove = ['es-ES', 'de-DE', 'it-IT', 'nl-NL', 'fr-FR', 'pt-PT'];
-function cleanData(data, deleteKeys) {
-    // There is nothing to be done if `data` is not an object,
-    // but for example "user01" or "MALE".
-    if (typeof data != 'object')
-        return;
-    if (!data)
-        return; // null object
-    for (const key in data) {
-        if (deleteKeys.includes(key)) {
-            delete data[key];
-        }
-        else {
-            // If the key is not deleted from the current `data` object,
-            // the value should be check for black-listed keys.
-            cleanData(data[key], localesToRemove);
-        }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { promises as fs } from 'fs';
+// import * as core from '@actions/core'
+const core = require('@actions/core');
+main().catch((error) => core.setFailed(error.message));
+const readFile = (file) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        return yield fs.readFile(file, 'utf8');
     }
+    catch (err) {
+        console.log(err);
+    }
+});
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const filePath = core.getInput('path', { required: true });
+            const localesToRemove = core.getInput('localesToRemove', { required: true });
+            const localesToRemoveJSON = JSON.parse(localesToRemove);
+            const file = readFile(filePath);
+            function cleanData(data, deleteKeys) {
+                // There is nothing to be done if `data` is not an object,
+                // but for example "user01" or "MALE".
+                if (typeof data != 'object')
+                    return;
+                if (!data)
+                    return; // null object
+                for (const key in data) {
+                    if (deleteKeys.includes(key)) {
+                        delete data[key];
+                    }
+                    else {
+                        // If the key is not deleted from the current `data` object,
+                        // the value should be check for black-listed keys.
+                        cleanData(data[key], localesToRemoveJSON);
+                    }
+                }
+            }
+            cleanData(file, localesToRemoveJSON);
+            fs.writeFile('contentful-export-yqiccqy-master-2.json', JSON.stringify(file));
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
 }
-cleanData(exportData, localesToRemove);
-fs.writeFileSync('contentful-export-yqiccqy-master-2.json', JSON.stringify(exportData));
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,48 @@
-const io = require('@actions/io')
-const fs = require('fs')
+import { promises as fs } from 'fs'
+import * as core from '@actions/core'
+// const core = require('@actions/core')
 
-let rawData = fs.readFileSync('contentful-export-yqiccqy-master.json')
-let exportData = JSON.parse(rawData)
+main().catch((error: any) => core.setFailed(error.message))
 
-const localesToRemove = ['es-ES', 'de-DE', 'it-IT', 'nl-NL', 'fr-FR', 'pt-PT']
-
-function cleanData(data: any, deleteKeys: string[]) {
-  // There is nothing to be done if `data` is not an object,
-  // but for example "user01" or "MALE".
-  if (typeof data != 'object') return
-  if (!data) return // null object
-
-  for (const key in data) {
-    if (deleteKeys.includes(key)) {
-      delete data[key]
-    } else {
-      // If the key is not deleted from the current `data` object,
-      // the value should be check for black-listed keys.
-      cleanData(data[key], localesToRemove)
-    }
+const readFile = async (file: string) => {
+  try {
+    return await fs.readFile(file, 'utf8')
+  } catch (err) {
+    console.log(err)
   }
 }
 
-cleanData(exportData, localesToRemove)
+async function main() {
+  try {
+    const filePath = core.getInput('path', { required: true })
+    const localesToRemove = core.getInput('localesToRemove', { required: true })
+    const localesToRemoveJSON = JSON.parse(localesToRemove)
 
-fs.writeFileSync(
-  'contentful-export-yqiccqy-master-2.json',
-  JSON.stringify(exportData)
-)
+    const file = readFile(filePath)
+
+    function cleanData(data: any, deleteKeys: string[]) {
+      // There is nothing to be done if `data` is not an object
+      if (typeof data != 'object') return
+      if (!data) return // null object
+
+      for (const key in data) {
+        if (deleteKeys.includes(key)) {
+          delete data[key]
+        } else {
+          // If the key is not deleted from the current `data` object,
+          // the value should be check for black-listed keys.
+          cleanData(data[key], localesToRemoveJSON)
+        }
+      }
+    }
+
+    cleanData(file, localesToRemoveJSON)
+
+    fs.writeFile(
+      'contentful-export-yqiccqy-master-2.json',
+      JSON.stringify(file)
+    )
+  } catch (error: any) {
+    core.setFailed(error.message)
+  }
+}
